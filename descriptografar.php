661bcacd883af0c7fea71f5fd8c7fe36fe2d58b5
Dhyego Palácios Bonifácio <dhyego@gmail.com>
2016-01-20 16:43:00 -0200

    Etapa 3 - Descriptografar sem código 1337

--- descriptografar.php
+++ descriptografar.php
@@ -7,14 +7,8 @@
 		public static function decriptar($str) {
 			$v = str_split($str);
 			foreach ($v as $k => $c) {
-				if (preg_match("/[a-z]/", $c)) {
+				if (preg_match("/[a-z]/", $c))
 					$v[$k] = chr(((ord($c)-96+self::$chave)%26)+96);
-				}
-				elseif (preg_match("/[0-9]/", $c)) {
-					if ($c == 0) $v[$k] = "o";
-					elseif ($c == 3) $v[$k] = "e";
-					elseif ($c == 4) $v[$k] = "a";
-				}
 			}
 			return implode($v);
 		}
